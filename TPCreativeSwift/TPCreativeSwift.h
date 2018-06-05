c0314c421e78ee8f690c42895f74190cb9a8305f
TPhong <tpalwayscreative@gmail.com>
2018-06-05 12:08:30 +0700

    Initial Commit

---
+++ TPCreativeSwift/TPCreativeSwift.h
@@ -0,0 +1,19 @@
+//
+//  TPCreativeSwift.h
+//  TPCreativeSwift
+//
+//  Created by Mac10 on 6/5/18.
+//  Copyright © 2018 Mac10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPCreativeSwift.
+FOUNDATION_EXPORT double TPCreativeSwiftVersionNumber;
+
+//! Project version string for TPCreativeSwift.
+FOUNDATION_EXPORT const unsigned char TPCreativeSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPCreativeSwift/PublicHeader.h>
+
+
